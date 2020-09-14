64b566138d220b9a4fe6015617117a756816e3f9
Xavier Stouder <xavier@stouder.io>
2020-09-14 17:49:06 +0200

    Revert "chore: install addons dependencies with a postinstall script instead of ace command"
    This reverts commit 2c637a7ae60857c5e4eb782f8c5bf4fdaaa08322.

--- commands/AddonsLoad.ts
+++ commands/AddonsLoad.ts
@@ -3,6 +3,7 @@ import { RcFile } from '@ioc:Adonis/Core/Application'
 
 import * as fs from 'fs/promises'
 import * as path from 'path'
+import * as cp from 'child_process'
 
 export default class AddonsLoad extends BaseCommand {
   public static commandName = 'addons:load'
@@ -15,8 +16,49 @@ export default class AddonsLoad extends BaseCommand {
     this.logger.info(`Found ${addons.length} addons`)
 
     await this.registerProviders(addons).catch(err => this.logger.error(`Error while registering addons providers : ${err}`))
+    await this.installDependencies(addons).catch(() => this.logger.error('Error while installing addons dependencies.'))
+    await this.unexcludeAddonsTs().catch(() => this.logger.error('Failed to unexclude addons folder.'))
 
     this.logger.success('Successfully loaded addons.')
+  }
+
+  private async installDependencies (addons: string[]) {
+    await Promise.all(addons.map(async addon => {
+      const addonManifestFile = await fs.readFile(
+        path.join('addons', addon, 'package.json'),
+        { encoding: 'utf-8' }
+      )
+      const addonManifest = JSON.parse(addonManifestFile)
+      const elnManifestFile = await fs.readFile('package.json', { encoding: 'utf-8' })
+      const elnManifest = JSON.parse(elnManifestFile)
+
+      // dependencies 
+      const elnDependenciesKeys = Object.keys(elnManifest.dependencies)
+      if(addonManifest.dependencies !== undefined) {
+        Object.keys(addonManifest.dependencies)
+          .filter(dependencyKey => elnDependenciesKeys.includes(dependencyKey) === false)
+          .forEach(dependencyKey => elnManifest.dependencies[dependencyKey] = addonManifest.dependencies[dependencyKey])
+      }
+
+      // dev dependencies 
+      const elnDevDependenciesKeys = Object.keys(elnManifest.devDependencies)
+      if(addonManifest.devDependencies !== undefined) {
+        Object.keys(addonManifest.devDependencies)
+          .filter(devDependencyKey => elnDevDependenciesKeys.includes(devDependencyKey) === false)
+          .forEach(devDependencyKey =>
+            elnManifest.devDependencies[devDependencyKey] = addonManifest.devDependencies[devDependencyKey]
+          )
+      }
+
+      await fs.writeFile('package.json', JSON.stringify(elnManifest, null, 2))
+      this.logger.info('Merged addons dependencies')
+      this.logger.info('Installing dependencies')
+      const result = cp.spawnSync('npm.cmd', ['install'], { cwd: '.' })
+      if(result.status !== 0) {
+        throw new Error('Fail to install dependencies')
+      }
+    }))
+    this.logger.success('Installed addons dependencies')
   }
 
   private async registerProviders (addons: string[]) {
@@ -37,5 +79,11 @@ export default class AddonsLoad extends BaseCommand {
     )
     await fs.writeFile('.adonisrc.json', JSON.stringify(rc, null, 2))
     this.logger.success('Registered addons providers')
+  }
+
+  private async unexcludeAddonsTs() {
+    const tsConfig = JSON.parse(await fs.readFile('tsconfig.json', { encoding: 'utf-8' }))
+    tsConfig.exclude = tsConfig.exclude.filter(excluded => excluded !== 'addons')
+    await fs.writeFile('tsconfig.json', JSON.stringify(tsConfig, null, 2))
   }
 }
