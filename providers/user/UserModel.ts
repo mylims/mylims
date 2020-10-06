557e9353cddc29577c581310ffc6484ea25e8fc3
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-10-06 13:28:23 +0200

    feat(user): add base fields for user

--- providers/user/UserModel.ts
+++ providers/user/UserModel.ts
@@ -1,5 +1,15 @@
 import { Model } from '@ioc:Mongodb/Model';
 
+interface AuthMethods {
+  ldap?: string;
+}
+
 export function UserBuilder(model: typeof Model) {
-  return class User extends model {};
+  return class User extends model {
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public role: 'ADMIN' | 'MEMBER';
+    public auth: AuthMethods;
+  };
 }
