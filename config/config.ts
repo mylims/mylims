b98257f507d99416a8ce9193d2ffc9402ea24794
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-10-08 17:58:39 +0200

    feat: validate config on import

---
+++ config/config.ts
@@ -0,0 +1,34 @@
+import { config } from './config.json';
+
+export interface ConfigProps {
+  date: string;
+  app: {
+    appKey: string;
+    appName: string;
+    logLevel: string;
+  };
+  auth: {
+    default: 'ldap' | 'user';
+    ldap: {
+      id: string;
+      uid: string;
+      url: string;
+      appDN: string;
+      appPassword: string;
+      baseUserDN: string;
+    };
+  };
+  mongodb: {
+    url: string;
+    database: string;
+  };
+  session: {
+    driver: 'cookie' | 'file' | 'redis';
+    cookieName: string;
+    sessionAge?: string;
+  };
+}
+
+export function getConfig<T extends keyof ConfigProps>(key: T): ConfigProps[T] {
+  return (config as ConfigProps)[key];
+}
