53a1c0047036b4b8f56c7b395dc6d0ad93b62a36
Michaël Zasso <targos@protonmail.com>
2020-10-13 16:05:47 +0200

    make env validation work

--- server.ts
+++ server.ts
@@ -16,7 +16,5 @@ import sourceMapSupport from 'source-map-support';
 
 sourceMapSupport.install({ handleUncaughtExceptions: false });
 
-Error.stackTraceLimit = 50;
-
 // eslint-disable-next-line no-console
 new Ignitor(__dirname).httpServer().start().catch(console.error);
