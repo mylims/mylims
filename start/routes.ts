7806a690183ecbd6f08cb09edc0850538bbb5641
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-10-13 11:47:11 +0200

    chore: organize dependencies and comments

--- start/routes.ts
+++ start/routes.ts
@@ -7,32 +7,32 @@ import { registerRoutes } from 'App/AddonsManager';
 import Credential from 'App/Models/CredentialModel';
 import User from 'App/Models/UserModel';
 
-Route.get('/', async () => {
-  return { hello: 'world' };
-});
+Route.get('/', async () => ({ hello: 'world' }));
 
-Route.post('/user', async ({ request }) => {
-  const { lastname, firstname, email } = request.all();
-  const credential = await Credential.create({
-    resetToken: uuid(),
+// Local user authentication
+Route.group(() => {
+  Route.post('/', async ({ request }) => {
+    const { lastname, firstname, email } = request.all();
+    const credential = await Credential.create({ resetToken: uuid() });
+    await User.create({
+      lastname,
+      firstname,
+      email,
+      auth: { local: credential._id },
+    });
+    return true;
   });
-  await User.create({
-    lastname,
-    firstname,
-    email,
-    auth: { local: credential._id },
-  });
-  return true;
-});
 
-Route.post('/user/password', async ({ request }) => {
-  const { token, password } = request.all();
-  const credentials = await Credential.findOne({ resetToken: token });
-  if (credentials === null) throw new Error('bad token');
-  credentials.resetToken = null;
-  credentials.hash = await Hash.make(password);
-  return credentials.save();
-});
+  Route.post('/password', async ({ request }) => {
+    const { token, password } = request.all();
+    const credentials = await Credential.findOne({ resetToken: token });
+    if (credentials === null) throw new Error('bad token');
+    credentials.resetToken = null;
+    credentials.hash = await Hash.make(password);
+    return credentials.save();
+  });
+}).prefix('/user');
+
 // Super admin views
 Route.group(() => {
   Route.get('/', 'AdminsController.login');
