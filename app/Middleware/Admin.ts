ceeff53bb958cbbe412798be05f881b0a3b06cba
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-10-19 13:46:22 +0200

    fix: remove value from session if it's not valid

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -1,11 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class Admin {
+  private readonly cookieName = 'sudo';
+
   public async handle(
     { session, response }: HttpContextContract,
     next: () => Promise<void>,
   ) {
-    const cookie = session.get('sudo');
+    const cookie = session.get(this.cookieName);
     if (!cookie) return response.redirect('/admin/error/session');
 
     // Checks time is valid
@@ -14,6 +16,7 @@ export default class Admin {
     const diffMilSec = currDate.getTime() - sessionDate.getTime();
     const windowTime = 1000 * 60 * 20; // 20 mins
     if (diffMilSec < 0 || diffMilSec >= windowTime) {
+      session.forget(this.cookieName);
       return response.redirect('/admin/error/session');
     }
     await next();
