bda767c0d26763443ede06039325fbff90e81abe
Xavier Stouder <xavier@stouder.io>
2020-10-19 15:05:00 +0200

    chore: enable request id

--- config/app.ts
+++ config/app.ts
@@ -73,7 +73,7 @@ export const http: HttpConfig = {
   | HTTP request and set it as `x-request-id` header.
   |
   */
-  generateRequestId: false,
+  generateRequestId: true,
 
   /*
   |--------------------------------------------------------------------------
