f1f0e9d8e6239081d702bb0a9880037913cbac11
Michaël Zasso <targos@protonmail.com>
2020-10-20 17:02:14 +0200

    fix: create config.json if it doesn't exist (#52)

--- app/AppConfig.ts
+++ app/AppConfig.ts
@@ -3,10 +3,35 @@ import fs from 'fs';
 import path from 'path';
 import { isDeepStrictEqual } from 'util';
 
-const { config, history } = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'config.json'), 'utf-8'),
-);
+const defaultConfig: ConfigFile = {
+  config: {
+    appKey: randomString(32),
+    date: '2020-10-07T14:48:00.000Z',
+    ldap: {
+      id: 'uid',
+      uid: 'uid',
+      url: 'ldap://127.0.0.1:1389',
+      appDN: 'cn=admin,dc=example,dc=org',
+      appPassword: 'adminpassword',
+      baseUserDN: 'ou=users,dc=example,dc=org',
+    },
+    mongodb: { url: 'mongodb://localhost:27017', database: 'mylims' },
+    session: { sessionAge: '2h' },
+  },
+  history: [],
+};
 
+const configPath = path.join(__dirname, 'config.json');
+
+if (fs.existsSync(configPath) === false) {
+  fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+}
+const { config, history } = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+
+export interface ConfigFile {
+  config: ConfigProps;
+  history: ConfigProps[];
+}
 export interface ConfigProps {
   date: string;
   appKey: string;
@@ -52,14 +77,13 @@ export function setConfig<T extends keyof ConfigProps>(
         ...config,
         [key]: newConfig,
         date: new Date().toISOString(),
-        appKey: randomString(32),
       },
       history: [config, ...history],
     };
 
     fs.writeFileSync(
       path.join(__dirname, 'config.json'),
-      JSON.stringify(confToSave),
+      JSON.stringify(confToSave, null, 2),
     );
   }
 }
