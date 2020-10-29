721cb8a4b6c3b879d9addcf50bc9d275dd7c9cc7
Xavier Stouder <xavier@stouder.io>
2020-10-29 15:47:53 +0100

    feat: rework UserManager (#39)

--- providers/internal-user/UserManager.ts
+++ providers/internal-user/UserManager.ts
@@ -1,9 +1,27 @@
 import User from 'App/Models/User';
 
 export default class UserManager {
-  public async getUser(authProvider: string, id: unknown) {
-    const user = await User.findOne({ [`authMethods.${authProvider}`]: id });
-    return user || this.createUser(authProvider, id);
+  public async getUser(authProvider: string, id: unknown, email?: string) {
+    const userByUid = await this.getUserByUID(authProvider, id);
+    if (userByUid !== null) return userByUid;
+
+    if (email !== undefined) {
+      const userByEmail = await this.getUserByEmail(email);
+      if (userByEmail !== null) return userByEmail;
+    }
+
+    return this.createUser(authProvider, id);
+  }
+
+  private getUserByUID(
+    authProvider: string,
+    id: unknown,
+  ): Promise<User | null> {
+    return User.findOne({ [`authMethods.${authProvider}`]: id });
+  }
+
+  private getUserByEmail(email: string): Promise<User | null> {
+    return User.findOne({ emails: email });
   }
 
   private createUser(authProvider: string, id: unknown) {
