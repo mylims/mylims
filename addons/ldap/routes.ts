c62c8a05e5f67e1a19472ea6d86bd4aa6d08d02c
Xavier Stouder <xavier@stouder.io>
2020-11-10 15:39:39 +0100

    fix

--- addons/ldap/routes.ts
+++ addons/ldap/routes.ts
@@ -1,13 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Route from '@ioc:Adonis/Core/Route';
 
-import LoginValidator from 'App/Validators/LoginValidator';
+import LoginValidator from './Validators/LoginValidator';
 
 Route.post(
   '/login',
   async ({ request, auth, response }: HttpContextContract) => {
-    const { email, password } = await request.validate(LoginValidator);
-    const result = await auth.use('ldap').login(email, password);
+    const { uid, password } = await request.validate(LoginValidator);
+    const result = await auth.use('ldap').login(uid, password);
     if (result === null || result === false) {
       return response.unauthorized({
         errors: [{ message: 'Bad credentials' }],
