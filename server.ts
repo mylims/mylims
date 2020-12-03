d0c79e3cd2f8ebf31ac95cdf93640afeada371b0
Xavier Stouder <xavier@stouder.io>
2020-12-03 14:35:42 +0100

    chore: bump eslint-config version

--- server.ts
+++ server.ts
@@ -39,17 +39,14 @@ function parent() {
 function child() {
   const httpServer = new Ignitor(__dirname).httpServer();
   httpServer
-    .start()
-    // eslint-disable-next-line no-console
-    .catch(console.error);
+    .start();
 }
 
 function starter() {
   if (process.env.CHILD) {
     child();
   } else {
-    // eslint-disable-next-line no-console
-    parent().then(starter).catch(console.log);
+    parent().then(starter);
   }
 }
 starter();
