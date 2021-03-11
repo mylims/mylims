c11a1c0b2c046c605930df45ada4edc7f76a76e6
Xavier Stouder <xavier@stouder.io>
2021-03-11 11:05:50 +0100

    fix: remove useless routes

--- start/routes.ts
+++ start/routes.ts
@@ -58,22 +58,6 @@ Route.group(() => {
   Route.get('/logout', 'AuthController.logout');
 }).prefix('/auth');
 
-Route.group(() => {
-  Route.group(() => {
-    Route.get('/create', 'TestLocalAuthController.create');
-    Route.get('/login', 'TestLocalAuthController.login');
-    Route.get('/user', 'TestLocalAuthController.user');
-    Route.get('/logout', 'TestLocalAuthController.logout');
-  }).prefix('local');
-
-  Route.group(() => {
-    Route.get('/create', 'TestLdapAuthController.create');
-    Route.get('/login', 'TestLdapAuthController.login');
-    Route.get('/user', 'TestLdapAuthController.user');
-    Route.get('/logout', 'TestLdapAuthController.logout');
-  }).prefix('ldap');
-}).prefix('/test-auth');
-
 // Require routes from addons
 AddonsManager.registerRoutes(Route);
 
