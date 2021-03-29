e502e78af15095db96d9fa2e20374072aaafa2ca
Xavier Stouder <xavier@stouder.io>
2021-03-29 16:40:49 +0200

    fix: factorize code

--- addons/file-sync/Models/ImportConfig.ts
+++ addons/file-sync/Models/ImportConfig.ts
@@ -1,14 +1,9 @@
-import { Pattern, SyncOptions } from 'fs-synchronizer';
+import { SyncOptions } from 'fs-synchronizer';
 
 import { Model } from '@ioc:Mongodb/Model';
-
-interface FileSynchronizerOptions extends Omit<SyncOptions, 'root'> {
-  maxDepth: number;
-  patterns: Pattern[];
-}
 
 export default class ImportConfig extends Model {
   public root: string;
   public enabled: boolean;
-  public fileSynchronizerOptions: FileSynchronizerOptions;
+  public fileSynchronizerOptions: Omit<SyncOptions, 'root'>;
 }
