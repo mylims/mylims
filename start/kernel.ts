29e6c974cc4d34e1db66fcb264f5d6584d4f8125
Xavier Stouder <xavier@stouder.io>
2021-04-15 16:08:40 +0200

    fix: middleware import

--- start/kernel.ts
+++ start/kernel.ts
@@ -1,6 +1,9 @@
+import type { MiddlewareHandler } from '@ioc:Adonis/Core/Middleware';
 import Server from '@ioc:Adonis/Core/Server';
 
-Server.middleware.register(['Adonis/Core/BodyParser']);
+Server.middleware.register([
+  (() => import('@ioc:Adonis/Core/BodyParser')) as MiddlewareHandler,
+]);
 Server.middleware.registerNamed({
   auth: 'App/Middleware/Auth',
   silentAuth: 'App/Middleware/SilentAuth',
