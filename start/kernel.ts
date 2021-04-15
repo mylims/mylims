9d53a89a35c1a7022a699845bf92f76c997d91ef
Xavier Stouder <xavier@stouder.io>
2021-04-15 13:08:59 +0200

    chore: bump deps (#84)

--- start/kernel.ts
+++ start/kernel.ts
@@ -1,6 +1,6 @@
 import Server from '@ioc:Adonis/Core/Server';
 
-Server.middleware.register(['Adonis/Core/BodyParserMiddleware']);
+Server.middleware.register(['Adonis/Core/BodyParser']);
 Server.middleware.registerNamed({
   auth: 'App/Middleware/Auth',
   silentAuth: 'App/Middleware/SilentAuth',
