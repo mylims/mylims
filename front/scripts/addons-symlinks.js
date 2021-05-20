40fded1091c5602dbf43698e86152f08f31025e5
Xavier Stouder <xavier@stouder.io>
2021-05-20 11:11:43 +0200

    fix: use relative target in symlink script

--- front/scripts/addons-symlinks.js
+++ front/scripts/addons-symlinks.js
@@ -11,22 +11,23 @@ const addonFolders = fs.readdirSync(addonsRoot);
 const symlinkRoot = join('pages', 'eln', 'addons');
 
 console.log('clear previous symlinks...');
-fs.rmSync(symlinkRoot, { recursive: true });
+try {
+  fs.rmSync(symlinkRoot, { recursive: true });
+} catch (err) {
+  console.log('no previous symlink found...');
+}
 fs.mkdirSync(symlinkRoot);
 
 console.log(`found ${addonFolders.length} addons...`);
 const createdSymlinks = addonFolders.filter((addonFolder) => {
-  const absoluteTarget = resolve(
-    process.cwd(),
-    join(addonsRoot, addonFolder, 'pages'),
-  );
+  const relativeTarget = join(addonsRoot, addonFolder, 'pages');
 
   const symlinkPath = resolve(process.cwd(), join(symlinkRoot, addonFolder));
 
   try {
-    fs.accessSync(absoluteTarget);
-    fs.symlinkSync(absoluteTarget, symlinkPath);
-    console.log(`${symlinkPath} --> ${absoluteTarget}`);
+    fs.accessSync(relativeTarget);
+    fs.symlinkSync(relativeTarget, symlinkPath);
+    console.log(`${symlinkPath} --> ${relativeTarget}`);
     return true;
   } catch (err) {
     if (err.code !== 'ENOENT') {
