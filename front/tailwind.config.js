9f3a8118356b0ae3ba902c76e85a715d3e3d3be5
Xavier Stouder <xavier@stouder.io>
2021-06-01 11:20:50 +0200

    fix: remove custom config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,12 +1,5 @@
 'use strict';
 
-const commonWidth = {
-  '1/4': '25%',
-  '1/3': '33%',
-  '1/2': '50%',
-  '3/4': '75%',
-};
-
 module.exports = {
   presets: [
     require('@zakodium/tailwind-config')({
@@ -18,14 +11,11 @@ module.exports = {
   theme: {
     minWidth: {
       0: '0',
-      ...commonWidth,
+      '1/4': '25%',
+      '1/3': '33%',
+      '1/2': '50%',
+      '3/4': '75%',
       full: '100%',
-    },
-    maxWidth: {
-      ...commonWidth,
-    },
-    width: {
-      ...commonWidth,
     },
   },
 };
