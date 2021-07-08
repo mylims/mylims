6fb3e67093e79f9b5aa556d9caabe68eaceb82c4
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-07-08 14:42:05 +0200

    feat: create a tree directory system visualization

--- front/addons/fileSync/TableFilesSync.tsx
+++ front/addons/fileSync/TableFilesSync.tsx
@@ -1,39 +1,109 @@
-import { InboxIcon } from '@heroicons/react/outline';
-import React, { useMemo } from 'react';
+import {
+  InboxIcon,
+  DownloadIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@heroicons/react/outline';
+import React, { useMemo, useState } from 'react';
+import bytes from 'byte-size';
 
 import {
   Badge,
   BadgeVariant,
+  Button,
   Color,
+  Roundness,
   Table,
   Td,
   Th,
+  Variant,
 } from '../../components/tailwind-ui';
 
-import { FilesByConfigQuery, FileStatus } from './generated/graphql';
+import {
+  FilesByConfigQuery,
+  FileStatus,
+  SyncFileRevision,
+} from './generated/graphql';
 
 interface TableFilesSyncProps {
   data?: FilesByConfigQuery;
 }
 
+enum TreeType {
+  file = 'file',
+  dir = 'dir',
+}
 interface FileSync {
   id: string;
+  type: TreeType.file;
+  name: string;
+  size: number;
+  level: number;
   revisionId: string;
   countRevisions: number;
-  size: number;
-  relativePath: string;
   status: FileStatus;
 }
+interface DirSync {
+  id: string;
+  type: TreeType.dir;
+  name: string;
+  size: number;
+  level: number;
+  children: TreeSync[];
+}
+type TreeSync = FileSync | DirSync;
 
 export default function TableFilesSync({ data }: TableFilesSyncProps) {
-  const files = useMemo(
-    () =>
-      (data?.filesByConfig ?? []).map((item) => ({
-        ...item,
-        id: item.revisionId,
-      })),
-    [data],
-  );
+  const files = useMemo(() => {
+    let ans: TreeSync[] = [];
+    for (const file of data?.filesByConfig ?? []) {
+      const { relativePath, ...fileOther } = file;
+      const path = relativePath.split('\\');
+      const name = path[path.length - 1];
+      const leaf: FileSync = {
+        ...fileOther,
+        name,
+        id: file.relativePath,
+        type: TreeType.file,
+        level: path.length - 1,
+      };
+
+      if (path.length === 1) {
+        // Root file
+        ans.push(leaf);
+      } else {
+        // Nested file
+        let filesList = ans;
+        for (let i = 0; i < path.length - 1; i++) {
+          const currDir = path[i];
+          let index = filesList.findIndex(
+            ({ name, type }) => type === TreeType.dir && name === currDir,
+          );
+          if (index === -1) {
+            filesList.push({
+              id: currDir,
+              name: currDir,
+              type: TreeType.dir,
+              size: leaf.size,
+              children: [],
+              level: i,
+            });
+            filesList = (filesList[filesList.length - 1] as DirSync).children;
+          } else {
+            filesList[index].size += leaf.size;
+            filesList = (filesList[index] as DirSync).children;
+          }
+        }
+        filesList.push(leaf);
+      }
+    }
+    return ans.sort((a, b) => {
+      if (a.type === TreeType.dir && b.type === TreeType.file) return -1;
+      if (b.type === TreeType.dir && a.type === TreeType.file) return 1;
+      return a.name.localeCompare(b.name);
+    });
+  }, [data]);
+
   return (
     <Table
       tableClassName="table-fixed"
@@ -48,10 +118,11 @@ export default function TableFilesSync({ data }: TableFilesSyncProps) {
 function Header() {
   return (
     <tr>
-      <Th className="w-3/6">Relative path</Th>
+      <Th className="w-2/6">Relative path</Th>
       <Th className="w-1/6">Size</Th>
       <Th className="w-1/6">Status</Th>
       <Th className="w-1/6">Revisions</Th>
+      <Th className="w-1/6">Actions</Th>
     </tr>
   );
 }
@@ -70,20 +141,85 @@ function Empty() {
     </>
   );
 }
-function Row({ value }: { value: FileSync }) {
+function Row({ value }: { value: TreeSync }) {
+  return value.type === TreeType.file ? (
+    <FileRow value={value} />
+  ) : (
+    <DirRow value={value} />
+  );
+}
+
+function FileRow({ value }: { value: FileSync }) {
+  const size = bytes(value.size).toString();
   return (
     <tr>
-      <Td title={value.relativePath} className="truncate">
-        {value.relativePath}
+      <Td
+        title={value.name}
+        className="truncate"
+        style={{ paddingLeft: `${1.5 + 1.5 * value.level}rem` }}
+      >
+        {value.name}
       </Td>
-      <Td>{value.size}</Td>
+      <Td>{size}</Td>
       <Td>
         <FileStatusLabel status={value.status} />
       </Td>
       <Td>{value.countRevisions}</Td>
+      <Td>
+        <Button
+          color={Color.neutral}
+          roundness={Roundness.circular}
+          variant={Variant.secondary}
+          className="ml-2"
+          title="Download"
+        >
+          <DownloadIcon className="w-3 h-3" />
+        </Button>
+      </Td>
     </tr>
   );
 }
+
+function DirRow({ value }: { value: DirSync }) {
+  const [expanded, setExpanded] = useState(false);
+  const size = bytes(value.size).toString();
+  return (
+    <>
+      <tr>
+        <Td
+          title={value.name}
+          className="truncate"
+          style={{ paddingLeft: `${1.5 + 1.5 * value.level}rem` }}
+        >
+          {value.name}
+        </Td>
+        <Td>{size}</Td>
+        <Td> - </Td>
+        <Td> - </Td>
+        <Td>
+          <Button
+            color={Color.neutral}
+            roundness={Roundness.circular}
+            variant={Variant.secondary}
+            className="ml-2"
+            title="Expand"
+            onClick={() => setExpanded(!expanded)}
+          >
+            {expanded ? (
+              <ChevronUpIcon className="w-3 h-3" />
+            ) : (
+              <ChevronDownIcon className="w-3 h-3" />
+            )}
+          </Button>
+        </Td>
+      </tr>
+      {expanded
+        ? value.children.map((child) => <Row key={child.id} value={child} />)
+        : null}
+    </>
+  );
+}
+
 function FileStatusLabel({ status }: { status: FileStatus }) {
   let color: Color;
   switch (status) {
