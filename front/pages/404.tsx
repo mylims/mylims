51236afebd945e511e8e202279a635247f9ea33e
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-07-16 13:46:16 +0200

    style: not found in the general template

--- front/pages/404.tsx
+++ front/pages/404.tsx
@@ -1,8 +1,13 @@
 import { useRouter } from 'next/router';
 
+import ElnLayout from '../components/ElnLayout';
 import { PageNotFoundErrorPage } from '../components/tailwind-ui';
 
 export default function Custom404() {
   const router = useRouter();
   return <PageNotFoundErrorPage url={router.asPath} />;
 }
+
+Custom404.getLayout = (page: React.ReactNode) => (
+  <ElnLayout pageTitle="Path not found">{page}</ElnLayout>
+);
