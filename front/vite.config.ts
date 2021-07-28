8793a1df8a4654900b612f063c01ac898d15a221
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-07-28 10:35:23 +0200

    fix: remove babel and check build

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        fileSync: resolve(__dirname, 'src/addons/fileSync/pages/index.html'),
       },
     },
   },
