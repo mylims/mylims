a4a384520952b0b7bbdb9d5d1ac744265eff7b91
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-07-29 15:35:46 +0200

    chore: remove env variables

--- addons/file-sync/Models/FileSyncOption.ts
+++ addons/file-sync/Models/FileSyncOption.ts
@@ -1,19 +1,10 @@
-import { Pattern, SyncOptions } from 'fs-synchronizer';
+import { Pattern } from 'fs-synchronizer';
 
 import { ObjectId, BaseModel, field } from '@ioc:Zakodium/Mongodb/Odm';
 
 export interface ReadyCheck {
   name: string;
   value?: unknown;
-}
-
-export interface FileSyncOptionFields extends SyncOptions {
-  id: ObjectId;
-  root: string;
-  enabled: boolean;
-  maxDepth: number;
-  patterns: Pattern[];
-  readyChecks: ReadyCheck[];
 }
 
 export class FileSyncOption extends BaseModel {
