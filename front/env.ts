c4b4b6bef477341ef2b736304838d60749cef604
Daniel Kostro <kostro.d@gmail.com>
2021-08-02 10:27:05 +0200

    Update env.ts

--- front/env.ts
+++ front/env.ts
@@ -1,5 +1,5 @@
 if (!import.meta.env.VITE_PUBLIC_API_URL) {
-  throw new Error('Missing environment variable: NEXT_PUBLIC_API_URL');
+  throw new Error('Missing environment variable: VITE_PUBLIC_API_URL');
 }
 
 export const API_URL = import.meta.env.VITE_PUBLIC_API_URL as string;
