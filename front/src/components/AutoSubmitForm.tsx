0289f29ef5b284426bdf46a11d24d9021b498274
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-08-20 14:52:02 +0200

    fix: small errors

--- front/src/components/AutoSubmitForm.tsx
+++ front/src/components/AutoSubmitForm.tsx
@@ -5,7 +5,10 @@ export default function AutoSubmitForm() {
   const { submitForm, values } = useFormikContext();
 
   useEffect(() => {
-    const handler = setTimeout(() => submitForm(), 1000);
+    const handler = setTimeout(() => {
+      // eslint-disable-next-line no-console
+      submitForm().catch((error) => console.error(error));
+    }, 1000);
     return () => clearTimeout(handler);
   }, [submitForm, values]);
   return <div />;
