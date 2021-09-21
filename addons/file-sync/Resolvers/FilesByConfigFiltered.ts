4b1adc1fda1fc77830e6dad15ba021039b634a33
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-09-21 11:31:20 +0200

    feat: update mongodb to use query instead fetch cursor

--- addons/file-sync/Resolvers/FilesByConfigFiltered.ts
+++ addons/file-sync/Resolvers/FilesByConfigFiltered.ts
@@ -27,20 +27,18 @@ const resolvers: GqlResolvers = {
       } = sortBy || {};
       const sortField = field === 'filename' ? field : `revisions.0.${field}`;
 
-      let fileCursor = (await SyncFile.getCollection())
-        .find({
-          '_id.configId': new ObjectId(id),
-          'revisions.0.size': { $gte: minSize, $lte: maxSize },
-          'revisions.0.date': { $gte: minDate, $lte: maxDate },
-          'revisions.0.status': { $in: status },
-        })
-        .sort({ [sortField]: direction === GqlSortDirection.DESC ? -1 : 1 });
+      let fileCursor = SyncFile.query({
+        '_id.configId': new ObjectId(id),
+        'revisions.0.size': { $gte: minSize, $lte: maxSize },
+        'revisions.0.date': { $gte: minDate, $lte: maxDate },
+        'revisions.0.status': { $in: status },
+      }).sortBy(sortField, direction === GqlSortDirection.DESC ? -1 : 1);
       const totalCount = await fileCursor.count();
 
       if (skip) fileCursor = fileCursor.skip(skip);
       if (limit) fileCursor = fileCursor.limit(limit);
 
-      const files = (await fileCursor.toArray()).map<GqlSyncFileRevision>(
+      const files = (await fileCursor.all()).map<GqlSyncFileRevision>(
         ({ _id: { relativePath }, revisions, filename, path }) => {
           const { id, size, date, status } = revisions[0];
           const urlPath = 'addons/file-sync/file-content';
