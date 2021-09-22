4e7a8c8ca6b98eca2c0b50bf349f4af6fe579a59
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-09-22 12:30:33 +0200

    feat: add files routes

---
+++ app/Controllers/Http/FileController.ts
@@ -0,0 +1,53 @@
+import { createReadStream } from 'fs';
+
+import type { MultipartFileContract } from '@ioc:Adonis/Core/BodyParser';
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import DataDrive, { PutOptions } from '@ioc:DataDrive';
+
+import File from 'App/Models/File';
+import CreateFileValidator from 'App/Validators/CreateFileValidator';
+
+export default class FileController {
+  public async fetch({ request, response }: HttpContextContract) {
+    const id = request.param('id', null);
+    if (id === null) {
+      return response.badRequest({ errors: ['Missing field id'] });
+    }
+    const file = await File.find(id);
+    if (file === null) {
+      return response.notFound({ errors: [{ message: 'File not found' }] });
+    }
+
+    const content = DataDrive.drive('local').getStream(file);
+    response.header(
+      'Content-Disposition',
+      `attachment; filename="${file.filename}"`,
+    );
+    return response.stream(content);
+  }
+
+  public async create({ request, response }: HttpContextContract) {
+    try {
+      const params = await request.validate(CreateFileValidator);
+      const file = await this.moveFromMultipart(params.file, params.filename);
+      return response.ok(file);
+    } catch (error) {
+      return response.badRequest({ errors: [error] });
+    }
+  }
+
+  private async moveFromMultipart(
+    file: MultipartFileContract,
+    filename: string,
+    options?: PutOptions,
+  ) {
+    const { tmpPath } = file;
+    if (!tmpPath) throw new Error('File path is missing');
+
+    return DataDrive.drive('local').put(
+      filename,
+      createReadStream(tmpPath),
+      options,
+    );
+  }
+}
