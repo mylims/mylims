1838e24f846b58a3ffaba15977125576db5892b0
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-09-22 12:30:33 +0200

    feat: add files routes

---
+++ app/Models/File.ts
@@ -0,0 +1,9 @@
+import { BaseModel, field } from '@ioc:Zakodium/Mongodb/Odm';
+
+export default class File extends BaseModel {
+  @field()
+  public _id: string;
+  public filename: string;
+  public size: number;
+  public eventId?: string;
+}
---
+++ app/Validators/CreateFileValidator.ts
@@ -0,0 +1,12 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { schema } from '@ioc:Adonis/Core/Validator';
+
+export default class CreateFileValidator {
+  public constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    file: schema.file(),
+    filename: schema.string(),
+    eventId: schema.string.optional(),
+  });
+}
