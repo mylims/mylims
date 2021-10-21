951effdd14891fc747ddd0f133872e0ed2ee3cdb
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-10-21 14:54:39 +0200

    chore: add addons to build

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -3,19 +3,20 @@ import fs from 'fs/promises';
 import path from 'path';
 
 const npmCommand = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+const addonsPath = 'build/addons';
 
 function spawn(addon) {
   return new Promise((resolve, reject) => {
-    const process = cp.spawn(npmCommand, ['install'], {
-      cwd: path.join('addons', addon),
-    });
+    const cwd = path.join(addonsPath, addon);
+    const process = cp.spawn(npmCommand, ['ci'], { cwd });
     process.on('error', reject);
     process.on('exit', resolve);
   });
 }
 
 (async () => {
-  const addons = await fs.readdir('addons');
+  const addons = await fs.readdir(addonsPath);
+  console.log(`Preparing ${addons.length} addons...`);
   await Promise.all(addons.map((addon) => spawn(addon)));
   console.log('Done');
 })();
