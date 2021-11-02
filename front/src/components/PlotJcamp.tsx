765ad0ee8ab79ca1341cf625a7323303d1af5f72
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-11-02 12:45:27 +0100

    feat: add plot to transfer view

---
+++ front/src/components/PlotJcamp.tsx
@@ -0,0 +1,78 @@
+import { Analysis, fromJcamp, getReactPlotJSON } from 'common-spectrum';
+import React, { ReactNode, useMemo, useState } from 'react';
+import { Annotation, PlotObject } from 'react-plot';
+
+interface PlotJcampProps {
+  content: string | null;
+  initialQuery: PlotQuery;
+  children?: (analysis: Analysis, query: PlotQuery) => ReactNode;
+}
+
+export interface PlotQuery {
+  xLabel: string;
+  yLabel: string;
+  xUnits: string;
+  yUnits: string;
+  scale: 'log' | 'linear';
+  logFilter?: 'remove' | 'abs';
+}
+
+export function PlotJcamp({ content, initialQuery, children }: PlotJcampProps) {
+  const [query] = useState<PlotQuery>(initialQuery);
+  const analysis = useMemo(
+    () => (content !== null ? fromJcamp(content) : null),
+    [content],
+  );
+  const plotContent = useMemo(() => {
+    if (!analysis) return null;
+    const { scale, logFilter, ...restQuery } = query;
+
+    let data = getReactPlotJSON([analysis], restQuery, {
+      xAxis: { labelSpace: 30, label: `${query.xLabel} [${query.xUnits}]` },
+      yAxis: {
+        labelSpace: 60,
+        label: `${query.yLabel} [${query.yUnits}]`,
+        scale,
+      },
+      seriesViewportStyle: { stroke: 'black', strokeWidth: '2px' },
+      enforceGrowing: true,
+      content: { displayMarker: false },
+      dimensions: {
+        width: 600,
+        height: 500,
+        margin: { bottom: 50, left: 90, top: 20, right: 40 },
+      },
+    });
+
+    if (scale === 'log') {
+      switch (logFilter) {
+        case 'remove': {
+          data.content = data.content.map((element) => {
+            if (element.type === 'annotation') return element;
+            const { data: list, ...other } = element;
+            return { ...other, data: list.filter((point) => point.y > 0) };
+          });
+        }
+        case 'abs':
+        default: {
+          data.content = data.content.map((element) => {
+            if (element.type === 'annotation') return element;
+            const { data: list, ...other } = element;
+            return {
+              ...other,
+              data: list.map((point) => ({ ...point, y: Math.abs(point.y) })),
+            };
+          });
+        }
+      }
+    }
+
+    return data;
+  }, [analysis, query]);
+  if (!plotContent || !analysis) return null;
+  return (
+    <PlotObject plot={plotContent}>
+      {children && children(analysis, query)}
+    </PlotObject>
+  );
+}
--- front/src/pages/measurements/MeasurementDetail/B1505Transfer.tsx
+++ front/src/pages/measurements/MeasurementDetail/B1505Transfer.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, AlertType, Card } from '@/components/tailwind-ui';
 
+import { Annotation } from 'react-plot';
+import { PlotJcamp } from '@/components/PlotJcamp';
+
 interface B1505TransferProps {
   file: null | { filename: string; downloadUrl: string; size: number };
   derived: {
@@ -20,6 +23,14 @@ interface B1505TransferState {
   content: string | null;
   error: Error | null;
 }
+
+const dashedLine = { strokeWidth: 2, opacity: 0.75, strokeDasharray: '5,5' };
+const underline = (color: string) => ({
+  textDecoration: 'underline',
+  WebkitTextDecorationColor: color,
+  textDecorationColor: color,
+  textDecorationThickness: '2px',
+});
 
 export default function B1505Transfer({ file, derived }: B1505TransferProps) {
   const [{ content, error }, setState] = useState<B1505TransferState>({
@@ -50,23 +61,87 @@ export default function B1505Transfer({ file, derived }: B1505TransferProps) {
   }
   return (
     <Card.Body>
-      <div>
-        <div className="font-medium">File</div>
-        <div className="text-neutral-400">{file?.filename}</div>
-      </div>
-      <div>
-        <div className="font-medium">Threshold Voltage</div>
-        <div className="text-neutral-400">
-          {derived.thresholdVoltage.value} V
+      <div className="flex justify-around">
+        <div>
+          <PlotJcamp
+            content={content}
+            initialQuery={{
+              xLabel: 'Vg',
+              xUnits: 'V',
+              yLabel: 'Id_dens',
+              yUnits: 'A/mm',
+              scale: 'log',
+              logFilter: 'remove',
+            }}
+          >
+            {(analysis, query) => {
+              if (query.xLabel === 'Vg' && query.yLabel === 'Id_dens') {
+                const spectrum = analysis.getXYSpectrum(query);
+                const { x, y } = spectrum?.variables || {};
+                return (
+                  <>
+                    <Annotation.Line
+                      x1={derived.thresholdVoltage.value}
+                      x2={derived.thresholdVoltage.value}
+                      y1={y.min ?? '0'}
+                      y2={y.max ?? '500'}
+                      style={{ stroke: 'green', ...dashedLine }}
+                    />
+                    <Annotation.Circle
+                      cx={derived.thresholdVoltage.value}
+                      cy={1e-6}
+                      r="4"
+                      style={{ fill: 'green' }}
+                    />
+                    {x && y && (
+                      <>
+                        <Annotation.Line
+                          x1={x.data[derived.subthresholdSlope.fromIndex]}
+                          x2={x.data[derived.subthresholdSlope.toIndex]}
+                          y1={y.data[derived.subthresholdSlope.fromIndex]}
+                          y2={y.data[derived.subthresholdSlope.toIndex]}
+                          style={{ stroke: 'blue', ...dashedLine }}
+                        />
+                        <Annotation.Circle
+                          cx={x.data[derived.subthresholdSlope.fromIndex]}
+                          cy={y.data[derived.subthresholdSlope.fromIndex]}
+                          r="4"
+                          style={{ fill: 'blue' }}
+                        />
+                        <Annotation.Circle
+                          cx={x.data[derived.subthresholdSlope.toIndex]}
+                          cy={y.data[derived.subthresholdSlope.toIndex]}
+                          r="4"
+                          style={{ fill: 'blue' }}
+                        />
+                      </>
+                    )}
+                  </>
+                );
+              }
+              return null;
+            }}
+          </PlotJcamp>
+        </div>
+        <div>
+          <div>
+            <div className="font-medium" style={underline('green')}>
+              Threshold Voltage
+            </div>
+            <div className="text-neutral-400">
+              {derived.thresholdVoltage.value} V
+            </div>
+          </div>
+          <div>
+            <div className="font-medium" style={underline('blue')}>
+              Subthreshold slope
+            </div>
+            <div className="text-neutral-400">
+              {derived.subthresholdSlope.medianSlope * 1000} mV/dec
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <div className="font-medium">Subthreshold resistance</div>
-        <div className="text-neutral-400">
-          {derived.subthresholdSlope.medianSlope} Ohm
-        </div>
-      </div>
-      <div className="truncate">{content}</div>
     </Card.Body>
   );
 }
