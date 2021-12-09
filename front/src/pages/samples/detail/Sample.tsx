990a3f9e084e264aa0805d6de19dc348f13a921a
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-12-09 15:09:20 +0100

    feat: add sample detail

---
+++ front/src/pages/samples/detail/Sample.tsx
@@ -0,0 +1,34 @@
+import ElnLayout from '@/components/ElnLayout';
+import FieldDescription from '@/components/FieldDescription';
+import BaseDetail from '@/pages/samples/detail/Default';
+import React from 'react';
+
+export default function SampleDetail() {
+  return (
+    <BaseDetail>
+      {(sample) => (
+        <div className="grid grid-cols-3 gap-4">
+          <FieldDescription title="Name">
+            {sample.sampleCode.join('_')}
+          </FieldDescription>
+          <FieldDescription title="Project">
+            {sample.project ?? '-'}
+          </FieldDescription>
+          <FieldDescription title="Purpose">
+            {sample.meta.purpose ?? '-'}
+          </FieldDescription>
+          <FieldDescription title="Description">
+            {sample.description ?? '-'}
+          </FieldDescription>
+          <FieldDescription title="Heterostructure">
+            {sample.meta.heterostructure ?? '-'}
+          </FieldDescription>
+        </div>
+      )}
+    </BaseDetail>
+  );
+}
+
+SampleDetail.getLayout = (page: React.ReactNode) => {
+  return <ElnLayout pageTitle="Sample detail">{page}</ElnLayout>;
+};
---
+++ front/src/pages/samples/list/Sample.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { EyeIcon, InformationCircleIcon } from '@heroicons/react/outline';
+
+import {
+  Button,
+  Card,
+  Color,
+  Roundness,
+  Size,
+  Variant,
+} from '@/components/tailwind-ui';
+import { Table as TableQuery } from '@/components/TableQuery';
+import ElnLayout from '@/components/ElnLayout';
+
+import SamplesList from './Default';
+import FieldDescription from '@/components/FieldDescription';
+import { Sample } from '@/generated/graphql';
+
+export default function SampleList() {
+  const [state, setState] = useState<Sample | null>(null);
+
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <div className="col-span-3">
+        <SamplesList kind="sample">
+          <TableQuery.TextColumn
+            title="heterostructure"
+            dataPath="meta.heterostructure"
+            disableSearch
+          />
+          <TableQuery.ActionsColumn>
+            {(row) => (
+              <Button
+                title="preview"
+                color={Color.success}
+                roundness={Roundness.circular}
+                variant={
+                  row.id === state?.id ? Variant.primary : Variant.secondary
+                }
+                className="ml-2"
+                onClick={() => setState(row as Sample)}
+              >
+                <EyeIcon className="w-5 h-5" />
+              </Button>
+            )}
+          </TableQuery.ActionsColumn>
+        </SamplesList>
+      </div>
+      <div>
+        <Button
+          className="mb-4"
+          variant={Variant.secondary}
+          color={Color.success}
+        >
+          + New sample
+        </Button>
+
+        <Card>
+          <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
+            <span className="text-xs font-semibold tracking-wider text-left uppercase">
+              Preview
+            </span>
+          </Card.Header>
+          <Card.Body>
+            {!state ? (
+              <p className="text-center text-neutral-500">
+                Select a wafer to preview
+              </p>
+            ) : (
+              <div className="grid grid-cols-2 gap-4">
+                <Link title="detail" to={`/sample/detail/sample/${state.id}`}>
+                  <Button
+                    className="flex space-x-2"
+                    color={Color.primary}
+                    variant={Variant.secondary}
+                  >
+                    <InformationCircleIcon className="w-5 h-5" />
+                    <span>Detail</span>
+                  </Button>
+                </Link>
+                {state.parent && (
+                  <Link
+                    title="detail"
+                    to={`/sample/detail/wafer/${state.parent.id}`}
+                  >
+                    <Button
+                      className="flex space-x-2"
+                      color={Color.primary}
+                      variant={Variant.secondary}
+                    >
+                      <InformationCircleIcon className="w-5 h-5" />
+                      <span>Parent</span>
+                    </Button>
+                  </Link>
+                )}
+
+                <FieldDescription title="Name">
+                  {state.sampleCode.join('_')}
+                </FieldDescription>
+                <FieldDescription title="Project">
+                  {state.project ?? '-'}
+                </FieldDescription>
+                <FieldDescription title="Purpose">
+                  {state.meta.purpose ?? '-'}
+                </FieldDescription>
+                <FieldDescription title="Description">
+                  {state.description ?? '-'}
+                </FieldDescription>
+                <FieldDescription title="Heterostructure">
+                  {state.meta.heterostructure ?? '-'}
+                </FieldDescription>
+              </div>
+            )}
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+SampleList.getLayout = (page: React.ReactNode) => (
+  <ElnLayout pageTitle="Samples">{page}</ElnLayout>
+);
