73b9d997389924dd75001a46f16aa0d3b0dae3bf
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-02-03 15:40:37 +0100

    style: split elements to each member

--- front/src/components/Nav/RouteMenu.tsx
+++ front/src/components/Nav/RouteMenu.tsx
@@ -10,26 +10,27 @@ const ADMIN_ROUTES: RouteType[] = [
   {
     label: 'File synchronization',
     pathname: '/fileSync/list',
-    pathmatch: '/fileSync/**',
+    pathmatch: '/fileSync/*',
   },
-  { label: 'Events', pathname: '/event/list', pathmatch: '/event/**' },
-];
-const SAMPLES_ROUTES: RouteType[] = [
-  { label: 'Wafers', pathname: '/sample/list/wafer' },
-  { label: 'Samples', pathname: '/sample/list/sample' },
-  { label: 'Dyes', pathname: '/sample/list/dye' },
-  { label: 'Devices', pathname: '/sample/list/device' },
+  { label: 'Events', pathname: '/event/list', pathmatch: '/event/*' },
 ];
 
 export function RouteMenu() {
   return (
     <>
-      <Menu title="Elements" routes={SAMPLES_ROUTES} />
+      <RouteLink route={{ label: 'Wafers', pathname: '/sample/list/wafer' }} />
+      <RouteLink
+        route={{ label: 'Samples', pathname: '/sample/list/sample' }}
+      />
+      <RouteLink route={{ label: 'Dyes', pathname: '/sample/list/dye' }} />
+      <RouteLink
+        route={{ label: 'Devices', pathname: '/sample/list/device' }}
+      />
       <RouteLink
         route={{
           label: 'Measurements',
           pathname: '/measurement/list',
-          pathmatch: '/measurement/**',
+          pathmatch: '/measurement/*',
         }}
       />
       <Menu title="Admin" routes={ADMIN_ROUTES} />
