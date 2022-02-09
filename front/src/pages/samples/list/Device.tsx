ae24a68a5ae3c3d7cda0cbdcdeef8c81c2da0636
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-02-09 11:42:52 +0100

    chore: remove single creation for subsamples

--- front/src/pages/samples/list/Device.tsx
+++ front/src/pages/samples/list/Device.tsx
@@ -44,7 +44,7 @@ export default function DeviceList() {
                   className="ml-2"
                   onClick={() => setState(row as Sample)}
                 >
-                  <EyeIcon className="h-5 w-5" />
+                  <EyeIcon className="w-5 h-5" />
                 </Button>
               )}
             </TableQuery.ActionsColumn>
@@ -57,77 +57,65 @@ export default function DeviceList() {
           />
         </SamplesList>
       </div>
-      <div>
-        <Link to="/sample/form/device">
-          <Button
-            className="mb-4"
-            variant={Variant.secondary}
-            color={Color.success}
-            size={Size.small}
-          >
-            + New device
-          </Button>
-        </Link>
 
-        <Card>
-          <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
-            <span className="text-left text-xs font-semibold uppercase tracking-wider">
-              Preview
-            </span>
-          </Card.Header>
-          <Card.Body>
-            {!state ? (
-              <p className="text-center text-neutral-500">
-                Select a wafer to preview
-              </p>
-            ) : (
-              <div className="grid grid-cols-2 gap-4">
-                <Link title="detail" to={`../../detail/device/${state.id}`}>
+      <Card>
+        <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
+          <span className="text-xs font-semibold tracking-wider text-left uppercase">
+            Preview
+          </span>
+        </Card.Header>
+        <Card.Body>
+          {!state ? (
+            <p className="text-center text-neutral-500">
+              Select a wafer to preview
+            </p>
+          ) : (
+            <div className="grid grid-cols-2 gap-4">
+              <Link title="detail" to={`../../detail/device/${state.id}`}>
+                <Button
+                  className="flex space-x-2"
+                  color={Color.primary}
+                  variant={Variant.secondary}
+                >
+                  <InformationCircleIcon className="w-5 h-5" />
+                  <span>Detail</span>
+                </Button>
+              </Link>
+              {state.parent && (
+                <Link
+                  title="detail"
+                  to={`../../detail/wafer/${state.parent.id}`}
+                >
                   <Button
                     className="flex space-x-2"
                     color={Color.primary}
                     variant={Variant.secondary}
                   >
-                    <InformationCircleIcon className="h-5 w-5" />
-                    <span>Detail</span>
+                    <ChipIcon className="w-5 h-5" />
+                    <span>Wafer</span>
                   </Button>
                 </Link>
-                {state.parent && (
-                  <Link
-                    title="detail"
-                    to={`../../detail/wafer/${state.parent.id}`}
-                  >
-                    <Button
-                      className="flex space-x-2"
-                      color={Color.primary}
-                      variant={Variant.secondary}
-                    >
-                      <ChipIcon className="h-5 w-5" />
-                      <span>Wafer</span>
-                    </Button>
-                  </Link>
-                )}
+              )}
 
-                <FieldDescription title="Wafer name">
-                  {state.sampleCode[0]}
-                </FieldDescription>
-                <FieldDescription title="Sample name">
-                  {state.sampleCode[1]}
-                </FieldDescription>
-                <FieldDescription title="Dye name">
-                  {state.sampleCode[2]}
-                </FieldDescription>
-                <FieldDescription title="Device name">
-                  {state.sampleCode[3]}
-                </FieldDescription>
-                <FieldDescription title="Description">
-                  {state.description ?? '-'}
-                </FieldDescription>
-              </div>
-            )}
-          </Card.Body>
-        </Card>
-      </div>
+              <FieldDescription title="Wafer name">
+                {state.sampleCode[0]}
+              </FieldDescription>
+              <FieldDescription title="Sample name">
+                {state.sampleCode[1]}
+              </FieldDescription>
+              <FieldDescription title="Dye name">
+                {state.sampleCode[2]}
+              </FieldDescription>
+              <FieldDescription title="Device name">
+                {state.sampleCode[3]}
+              </FieldDescription>
+              <FieldDescription title="Description">
+                {state.description ?? '-'}
+              </FieldDescription>
+            </div>
+          )}
+        </Card.Body>
+      </Card>
     </div>
   );
 }
--- front/src/pages/samples/list/Sample.tsx
+++ front/src/pages/samples/list/Sample.tsx
@@ -44,7 +44,7 @@ export default function SampleList() {
                   className="ml-2"
                   onClick={() => setState(row as Sample)}
                 >
-                  <EyeIcon className="h-5 w-5" />
+                  <EyeIcon className="w-5 h-5" />
                 </Button>
               )}
             </TableQuery.ActionsColumn>
@@ -57,80 +57,67 @@ export default function SampleList() {
           />
         </SamplesList>
       </div>
-      <div>
-        <Link to="/sample/form/sample">
-          <Button
-            className="mb-4"
-            variant={Variant.secondary}
-            color={Color.success}
-            size={Size.small}
-          >
-            + New sample
-          </Button>
-        </Link>
-
-        <Card>
-          <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
-            <span className="text-left text-xs font-semibold uppercase tracking-wider">
-              Preview
-            </span>
-          </Card.Header>
-          <Card.Body>
-            {!state ? (
-              <p className="text-center text-neutral-500">
-                Select a wafer to preview
-              </p>
-            ) : (
-              <div className="grid grid-cols-2 gap-4">
-                <Link title="detail" to={`../../detail/sample/${state.id}`}>
+      <Card>
+        <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
+          <span className="text-xs font-semibold tracking-wider text-left uppercase">
+            Preview
+          </span>
+        </Card.Header>
+        <Card.Body>
+          {!state ? (
+            <p className="text-center text-neutral-500">
+              Select a wafer to preview
+            </p>
+          ) : (
+            <div className="grid grid-cols-2 gap-4">
+              <Link title="detail" to={`../../detail/sample/${state.id}`}>
+                <Button
+                  className="flex space-x-2"
+                  color={Color.primary}
+                  variant={Variant.secondary}
+                >
+                  <InformationCircleIcon className="w-5 h-5" />
+                  <span>Detail</span>
+                </Button>
+              </Link>
+              {state.parent && (
+                <Link
+                  title="detail"
+                  to={`../../detail/wafer/${state.parent.id}`}
+                >
                   <Button
                     className="flex space-x-2"
                     color={Color.primary}
                     variant={Variant.secondary}
                   >
-                    <InformationCircleIcon className="h-5 w-5" />
-                    <span>Detail</span>
+                    <ChipIcon className="w-5 h-5" />
+                    <span>Wafer</span>
                   </Button>
                 </Link>
-                {state.parent && (
-                  <Link
-                    title="detail"
-                    to={`../../detail/wafer/${state.parent.id}`}
-                  >
-                    <Button
-                      className="flex space-x-2"
-                      color={Color.primary}
-                      variant={Variant.secondary}
-                    >
-                      <ChipIcon className="h-5 w-5" />
-                      <span>Wafer</span>
-                    </Button>
-                  </Link>
-                )}
+              )}
 
-                <FieldDescription title="Wafer name">
-                  {state.sampleCode[0]}
-                </FieldDescription>
-                <FieldDescription title="Sample name">
-                  {state.sampleCode[1]}
-                </FieldDescription>
-                <FieldDescription title="Project">
-                  {state.project ?? '-'}
-                </FieldDescription>
-                <FieldDescription title="Purpose">
-                  {state.meta.purpose ?? '-'}
-                </FieldDescription>
-                <FieldDescription title="Description">
-                  {state.description ?? '-'}
-                </FieldDescription>
-                <FieldDescription title="Heterostructure">
-                  {state.meta.heterostructure ?? '-'}
-                </FieldDescription>
-              </div>
-            )}
-          </Card.Body>
-        </Card>
-      </div>
+              <FieldDescription title="Wafer name">
+                {state.sampleCode[0]}
+              </FieldDescription>
+              <FieldDescription title="Sample name">
+                {state.sampleCode[1]}
+              </FieldDescription>
+              <FieldDescription title="Project">
+                {state.project ?? '-'}
+              </FieldDescription>
+              <FieldDescription title="Purpose">
+                {state.meta.purpose ?? '-'}
+              </FieldDescription>
+              <FieldDescription title="Description">
+                {state.description ?? '-'}
+              </FieldDescription>
+              <FieldDescription title="Heterostructure">
+                {state.meta.heterostructure ?? '-'}
+              </FieldDescription>
+            </div>
+          )}
+        </Card.Body>
+      </Card>
     </div>
   );
 }
