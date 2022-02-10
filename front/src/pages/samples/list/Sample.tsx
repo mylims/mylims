d8f5935d288ed9e5366e99fa25f5de4c97a6fb6f
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-02-10 17:27:25 +0100

    chore: prettier

--- front/src/pages/samples/list/Sample.tsx
+++ front/src/pages/samples/list/Sample.tsx
@@ -44,7 +44,7 @@ export default function SampleList() {
                   className="ml-2"
                   onClick={() => setState(row as Sample)}
                 >
-                  <EyeIcon className="w-5 h-5" />
+                  <EyeIcon className="h-5 w-5" />
                 </Button>
               )}
             </TableQuery.ActionsColumn>
@@ -59,7 +59,7 @@ export default function SampleList() {
       </div>
       <Card>
         <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
-          <span className="text-xs font-semibold tracking-wider text-left uppercase">
+          <span className="text-left text-xs font-semibold uppercase tracking-wider">
             Preview
           </span>
         </Card.Header>
@@ -76,7 +76,7 @@ export default function SampleList() {
                   color={Color.primary}
                   variant={Variant.secondary}
                 >
-                  <InformationCircleIcon className="w-5 h-5" />
+                  <InformationCircleIcon className="h-5 w-5" />
                   <span>Detail</span>
                 </Button>
               </Link>
@@ -90,7 +90,7 @@ export default function SampleList() {
                     color={Color.primary}
                     variant={Variant.secondary}
                   >
-                    <ChipIcon className="w-5 h-5" />
+                    <ChipIcon className="h-5 w-5" />
                     <span>Wafer</span>
                   </Button>
                 </Link>
--- front/src/pages/samples/list/Wafer.tsx
+++ front/src/pages/samples/list/Wafer.tsx
@@ -46,7 +46,7 @@ export default function WaferList() {
                   className="ml-2"
                   onClick={() => setState(row as Sample)}
                 >
-                  <EyeIcon className="w-5 h-5" />
+                  <EyeIcon className="h-5 w-5" />
                 </Button>
               )}
             </TableQuery.ActionsColumn>
@@ -106,7 +106,7 @@ export default function WaferList() {
 
         <Card>
           <Card.Header className="flex flex-row justify-between bg-neutral-50 text-neutral-500">
-            <span className="text-xs font-semibold tracking-wider text-left uppercase">
+            <span className="text-left text-xs font-semibold uppercase tracking-wider">
               Preview
             </span>
           </Card.Header>
@@ -117,7 +117,7 @@ export default function WaferList() {
               </p>
             ) : (
               <div>
-                <div className="w-full mb-2">
+                <div className="mb-2 w-full">
                   <ResponsiveChart minHeight={30}>
                     {({ width }) => <WaferDicing size={width} wafer={state} />}
                   </ResponsiveChart>
@@ -129,7 +129,7 @@ export default function WaferList() {
                       color={Color.primary}
                       variant={Variant.secondary}
                     >
-                      <InformationCircleIcon className="w-5 h-5" />
+                      <InformationCircleIcon className="h-5 w-5" />
                       <span>Detail</span>
                     </Button>
                   </Link>
@@ -150,7 +150,7 @@ export default function WaferList() {
                         color={Color.primary}
                         variant={Variant.secondary}
                       >
-                        <FolderOpenIcon className="w-5 h-5" />
+                        <FolderOpenIcon className="h-5 w-5" />
                         <span>Samples</span>
                       </Button>
                     </Link>
