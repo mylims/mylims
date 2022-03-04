4d43e88edfece7c18f25d61ebc1b8cd69e07a865
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-03-04 11:46:17 +0100

    fix: follow slate nesting structure

--- addons/powerlab-migrate/deserialize.ts
+++ addons/powerlab-migrate/deserialize.ts
@@ -1,32 +1,77 @@
 import { parse, HTMLElement, Node } from 'node-html-parser';
 import type { Descendant } from 'slate';
 
-import { FormattedText, ListItemElement } from './types';
+import { FormattedText, CustomElement, ListItemElement } from './types';
 
 function isHTML(node: HTMLElement | Node): node is HTMLElement {
   return node.nodeType === 1;
 }
-function isDescendant(descendant: Descendant | null): descendant is Descendant {
-  return descendant !== null;
+function isElement(node: Descendant): node is CustomElement {
+  return (node as CustomElement).type !== undefined;
+}
+function isListItemElement(node: Descendant): node is ListItemElement {
+  return (node as ListItemElement).type === 'list-item';
+}
+
+/**
+ * Filter the possible children of a node to only allows text nodes.
+ * @param children - List of possible nodes or edges
+ * @returns Flat list of nodes only
+ */
+function flatFormattedText(children: Descendant[]) {
+  let textChildren: FormattedText[] = [];
+  let elementChildren: CustomElement[] = [];
+  for (const child of children) {
+    if (isElement(child)) {
+      if (['paragraph', 'heading-one', 'heading-two'].includes(child.type)) {
+        const {
+          textChildren: childTextChildren,
+          elementChildren: childElementChildren,
+        } = flatFormattedText(child.children);
+        elementChildren.concat(childElementChildren);
+        textChildren = textChildren.concat(childTextChildren);
+      } else if (child.children.length > 0) {
+        elementChildren.push(child);
+      }
+    } else {
+      textChildren.push(child);
+    }
+  }
+  if (textChildren.length === 0) {
+    textChildren.push({ text: '' });
+  }
+  return { textChildren, elementChildren };
+}
+
+function deserializeChildren(nodes: Node[], imageUuid?: string): Descendant[] {
+  let result: Descendant[] = [];
+  for (const node of nodes) {
+    const parsed = deserialize(node, imageUuid);
+    if (parsed !== null) {
+      result = result.concat(parsed);
+    }
+  }
+  return result;
 }
 
 export function toSlate(html: string, imageUuid?: string) {
   const document = parse(html);
-  return Array.from(document.childNodes)
-    .map((node) => deserialize(node, imageUuid))
-    .filter(isDescendant);
+  const result = deserializeChildren(document.childNodes, imageUuid);
+  if (result.length === 0) return [{ text: '' }];
+  return result;
 }
 
 export function deserialize(
   el: HTMLElement | Node,
   imageUuid?: string,
-): Descendant | null {
-  if (!isHTML(el)) return { text: el.textContent ?? '' };
+): Descendant[] | null {
+  if (!isHTML(el)) {
+    const text = el.textContent?.replace(/(?<space>\r\n|\n|\r)/gm, '');
+    if (text === '') return null;
+    return [{ text }];
+  }
 
-  let children = Array.from(el.childNodes)
-    .map((node) => deserialize(node, imageUuid))
-    .filter(isDescendant);
-
+  let children = deserializeChildren(el.childNodes, imageUuid);
   if (children.length === 0) children = [{ text: '' }];
 
   switch (el.rawTagName) {
@@ -39,40 +84,63 @@ export function deserialize(
     }
     case 'p':
     case 'span': {
-      return { type: 'paragraph', children: children as FormattedText[] };
+      const { textChildren, elementChildren } = flatFormattedText(children);
+      return [
+        { type: 'paragraph', children: textChildren },
+        ...elementChildren,
+      ];
     }
     case 'strong': {
-      return {
-        type: 'paragraph',
-        children: (children as FormattedText[]).map((child) => ({
-          ...child,
-          bold: true,
-        })),
-      };
+      const { textChildren, elementChildren } = flatFormattedText(children);
+      return [
+        {
+          type: 'paragraph',
+          children: textChildren.map((child) => ({ ...child, bold: true })),
+        },
+        ...elementChildren,
+      ];
     }
     case 'h1': {
-      return { type: 'heading-one', children: children as FormattedText[] };
+      const { textChildren, elementChildren } = flatFormattedText(children);
+      return [
+        { type: 'heading-one', children: textChildren },
+        ...elementChildren,
+      ];
     }
     case 'h2': {
-      return { type: 'heading-two', children: children as FormattedText[] };
+      const { textChildren, elementChildren } = flatFormattedText(children);
+      return [
+        { type: 'heading-two', children: textChildren },
+        ...elementChildren,
+      ];
     }
     case 'li': {
-      return { type: 'list-item', children: children as FormattedText[] };
+      const { textChildren, elementChildren } = flatFormattedText(children);
+      return [
+        { type: 'list-item', children: textChildren },
+        ...elementChildren,
+      ];
     }
     case 'ul': {
-      return { type: 'bulleted-list', children: children as ListItemElement[] };
+      const listItems = children.filter(isListItemElement);
+      return [{ type: 'bulleted-list', children: listItems }];
     }
     case 'ol': {
-      return { type: 'numbered-list', children: children as ListItemElement[] };
+      const listItems = children.filter(isListItemElement);
+      return [{ type: 'numbered-list', children: listItems }];
     }
     case 'img': {
       if (!imageUuid) return null;
-      return { type: 'image', uuid: imageUuid, children: [{ text: '' }] };
+      return [{ type: 'image', uuid: imageUuid, children: [{ text: '' }] }];
     }
     default: {
       // It's missing the anchor tag, so we'll just return the text.
       // It's missing the table related tags
-      return { type: 'paragraph', children: children as FormattedText[] };
+      const { textChildren, elementChildren } = flatFormattedText(children);
+      return [
+        { type: 'paragraph', children: textChildren },
+        ...elementChildren,
+      ];
     }
   }
 }
