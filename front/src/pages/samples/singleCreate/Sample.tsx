b6abd6703c5c784b8dde8ebf460aab8bb9403c33
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-03-17 15:17:35 +0100

    feat: add single sample

--- front/src/pages/samples/singleCreate/Sample.tsx
+++ front/src/pages/samples/singleCreate/Sample.tsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import { API_URL } from '@/../env';
 import ElnLayout from '@/components/ElnLayout';
-import MultiSelect from '@/components/FormSchema/MultiSelect';
 import { RichTextFieldRHF } from '@/components/RichTextFieldRHF';
-import WaferDiameterField from '@/components/WaferDiameterField';
 import {
+  Alert,
+  AlertType,
   DropzoneFieldRHF,
   InputFieldRHF,
   optionalNumber,
   optionalString,
   requiredObject,
   requiredString,
+  Spinner,
+  ToggleFieldRHF,
 } from '@/components/tailwind-ui';
+import { useSampleQuery } from '@/generated/graphql';
 
 import DefaultCreate from './Default';
 
@@ -31,61 +35,48 @@ const waferCreateSchema = {
   mobility: optionalNumber(),
 };
 
-export default function WaferCreate() {
-  const [defaultCreation, setDefaultCreation] = useState(true);
+export default function SampleCreate() {
+  const { id = '' } = useParams<{ id: string }>();
+  const { data, loading, error } = useSampleQuery({ variables: { id } });
+  if (loading) return <Spinner className="h-10 w-10 text-danger-500" />;
+  if (error || !data) {
+    return (
+      <Alert title="Error while fetching sample" type={AlertType.ERROR}>
+        Unexpected error: {error?.message}
+      </Alert>
+    );
+  }
+  const { sample } = data;
+
   return (
     <DefaultCreate
-      codeLength={1}
+      codeLength={2}
       kind="wafer"
       metaSchema={waferCreateSchema}
-      defaultCreation={defaultCreation}
-      sampleCode={[]}
+      sampleCode={sample.sampleCode}
+      parent={sample.id}
     >
       <div className="my-2 flex flex-col md:grid md:grid-flow-col md:grid-rows-2 md:gap-4">
         <div className="col-span-2">
-          <div className="grid-cols-auto grid gap-4">
-            <InputFieldRHF name="sampleCode.0" label="Wafer name" required />
+          <div className="grid-cols-auto grid items-end gap-4">
+            <InputFieldRHF
+              name="sampleCode.0"
+              label="Wafer name"
+              required
+              disabled
+            />
+            <InputFieldRHF name="sampleCode.1" label="Sample name" required />
             <InputFieldRHF name="project" label="Project" />
-            <InputFieldRHF name="comment" label="Comment" />
-            <MultiSelect name="labels" label="Labels" />
             <InputFieldRHF name="meta.purpose" label="Purpose" />
+            <InputFieldRHF name="meta.labelPurpose" label="Label purpose" />
             <InputFieldRHF name="meta.heterostructure" label="EPI structure" />
-            <InputFieldRHF name="meta.substrate" label="Substrate" />
-            <InputFieldRHF name="meta.supplier" label="Supplier" />
-            <InputFieldRHF
-              name="meta.supplierWaferNumber"
-              label="Supplier wafer number"
-            />
-            <InputFieldRHF name="meta.placeOfGrowth" label="Place of growth" />
-            <InputFieldRHF name="meta.location" label="Location" />
-            <InputFieldRHF
-              name="meta.locationComment"
-              label="Location comment"
-            />
-            <InputFieldRHF name="meta.rs" label="Rs (Ohm/sq)" type="number" />
-            <InputFieldRHF name="meta.ns" label="Ns (e13/cm^2)" type="number" />
-            <InputFieldRHF
-              name="meta.mobility"
-              label="Mobility (cm^2/Vs)"
-              type="number"
-            />
+            <ToggleFieldRHF name="meta.reserved" label="Reserved" />
           </div>
         </div>
         <div className="col-span-2">
           <DropzoneFieldRHF label="Attachments" name="attachments" showList />
         </div>
         <div className="row-span-3">
-          <WaferDiameterField
-            name="meta.size"
-            label="Diameter"
-            defaultCreation={defaultCreation}
-            setDefaultCreation={setDefaultCreation}
-            options={['chip', '2 inch', '4 inch', '6 inch'].map((value) => ({
-              value,
-              label: value,
-            }))}
-            required
-          />
           <RichTextFieldRHF
             className="max-w-7xl"
             name="description"
@@ -110,6 +101,6 @@ export default function WaferCreate() {
   );
 }
 
-WaferCreate.getLayout = (page: React.ReactNode) => (
-  <ElnLayout pageTitle="Create new wafer">{page}</ElnLayout>
+SampleCreate.getLayout = (page: React.ReactNode) => (
+  <ElnLayout pageTitle="Create new sample">{page}</ElnLayout>
 );
