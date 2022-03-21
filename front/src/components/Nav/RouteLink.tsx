50e5d26200207d139a64148619e77f520bcb68ba
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-03-21 15:12:46 +0100

    style: highlight routes on subroutes

--- front/src/components/Nav/RouteLink.tsx
+++ front/src/components/Nav/RouteLink.tsx
@@ -6,16 +6,21 @@ import { RouteType } from './types';
 
 interface RouteLinkProps {
   route: RouteType;
+  couldHaveId?: boolean;
 }
 
-export default function RouteLink({ route }: RouteLinkProps) {
-  const isMatch = useMatch(route.pathmatch || route.pathname);
+export default function RouteLink({ route, couldHaveId }: RouteLinkProps) {
+  const match = route.pathmatch ?? route.pathname;
+  const isMatch = useMatch(match);
+  const isIdMatch = useMatch(`${match}/:id`);
+  const isActive = isMatch || (isIdMatch && couldHaveId);
+
   return (
     <Link
       to={route.pathname}
       className={clsx(
         'rounded-md px-3 py-2 text-sm font-medium focus:outline-none',
-        isMatch ? 'bg-neutral-900 text-neutral-100' : 'text-neutral-300',
+        isActive ? 'bg-neutral-900 text-neutral-100' : 'text-neutral-300',
       )}
     >
       {route.label}
