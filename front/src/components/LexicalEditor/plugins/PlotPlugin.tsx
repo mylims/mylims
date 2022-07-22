917e9e706a3b025b4a8dc9df543f0086c55c3da8
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-07-22 15:43:12 +0200

    feat: add plot node

---
+++ front/src/components/LexicalEditor/plugins/PlotPlugin.tsx
@@ -0,0 +1,30 @@
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { createCommand, LexicalCommand } from 'lexical';
+import { useEffect } from 'react';
+
+import { PlotNode } from '../nodes/PlotNode';
+
+export type InsertPlotPayload = Readonly<{
+  id: string;
+  type: string;
+  sampleCode: string;
+}>;
+export const INSERT_SAMPLE_LINK_COMMAND: LexicalCommand<InsertPlotPayload> =
+  createCommand();
+
+export interface PlotRef {
+  appendPlot(sampleCode: string): void;
+}
+export default function PlotPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    if (!editor.hasNodes([PlotNode])) {
+      throw new Error(
+        'PlotNode: PlotNode not registered on editor',
+      );
+    }
+  }, [editor]);
+
+  return null;
+}
