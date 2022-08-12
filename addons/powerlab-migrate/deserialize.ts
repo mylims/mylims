7e17ef3905150b8a55416107bd837f2fd41a69fa
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2022-08-12 14:41:31 +0200

    feat: migrating command moved from slate to lexical

--- addons/powerlab-migrate/deserialize.ts
+++ addons/powerlab-migrate/deserialize.ts
@@ -1,146 +1,46 @@
-import { parse, HTMLElement, Node } from 'node-html-parser';
-import type { Descendant } from 'slate';
+import { createHeadlessEditor } from '@lexical/headless';
+import { $generateNodesFromDOM } from '@lexical/html';
+import { ListItemNode, ListNode } from '@lexical/list';
+import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import { TableCellNode, TableNode, TableRowNode } from '@lexical/table';
+import { JSDOM } from 'jsdom';
+import { $createParagraphNode, $getRoot } from 'lexical';
 
-import { FormattedText, CustomElement, ListItemElement } from './types';
+import { ImageNode } from './ImageNode';
 
-function isHTML(node: HTMLElement | Node): node is HTMLElement {
-  return node.nodeType === 1;
-}
-function isElement(node: Descendant): node is CustomElement {
-  return (node as CustomElement).type !== undefined;
-}
-function isListItemElement(node: Descendant): node is ListItemElement {
-  return (node as ListItemElement).type === 'list-item';
-}
+export function toLexical(htmlString: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const editor = createHeadlessEditor({
+      namespace: 'lexical',
+      nodes: [
+        ImageNode,
+        ListItemNode,
+        ListNode,
+        HeadingNode,
+        QuoteNode,
+        TableCellNode,
+        TableNode,
+        TableRowNode,
+      ],
+      onError: (error) => reject(error),
+    });
 
-/**
- * Filter the possible children of a node to only allows text nodes.
- * @param children - List of possible nodes or edges
- * @returns Flat list of nodes only
- */
-function flatFormattedText(children: Descendant[]) {
-  let textChildren: FormattedText[] = [];
-  let elementChildren: CustomElement[] = [];
-  for (const child of children) {
-    if (isElement(child)) {
-      if (['paragraph', 'heading-one', 'heading-two'].includes(child.type)) {
-        const {
-          textChildren: childTextChildren,
-          elementChildren: childElementChildren,
-        } = flatFormattedText(child.children);
-        elementChildren.concat(childElementChildren);
-        textChildren = textChildren.concat(childTextChildren);
-      } else if (child.children.length > 0) {
-        elementChildren.push(child);
-      }
-    } else {
-      textChildren.push(child);
-    }
-  }
-  if (textChildren.length === 0) {
-    textChildren.push({ text: '' });
-  }
-  return { textChildren, elementChildren };
-}
-
-function deserializeChildren(nodes: Node[], imageUuid?: string): Descendant[] {
-  let result: Descendant[] = [];
-  for (const node of nodes) {
-    const parsed = deserialize(node, imageUuid);
-    if (parsed !== null) {
-      result = result.concat(parsed);
-    }
-  }
-  return result;
-}
-
-export function toSlate(html: string, imageUuid?: string) {
-  const document = parse(html);
-  const result = deserializeChildren(document.childNodes, imageUuid);
-  if (result.length === 0) return [{ text: '' }];
-  return result;
-}
-
-export function deserialize(
-  el: HTMLElement | Node,
-  imageUuid?: string,
-): Descendant[] | null {
-  if (!isHTML(el)) {
-    const text = el.textContent?.replace(/(?<space>\r\n|\n|\r)/gm, '');
-    if (text === '') return null;
-    return [{ text }];
-  }
-
-  let children = deserializeChildren(el.childNodes, imageUuid);
-  if (children.length === 0) children = [{ text: '' }];
-
-  switch (el.rawTagName) {
-    case 'br':
-    case 'col':
-    case 'colgroup':
-    case 'font':
-    case 'em': {
-      return null;
-    }
-    case 'p':
-    case 'span': {
-      const { textChildren, elementChildren } = flatFormattedText(children);
-      return [
-        { type: 'paragraph', children: textChildren },
-        ...elementChildren,
-      ];
-    }
-    case 'strong': {
-      const { textChildren, elementChildren } = flatFormattedText(children);
-      return [
-        {
-          type: 'paragraph',
-          children: textChildren.map((child) => ({ ...child, bold: true })),
+    editor.update(
+      () => {
+        const dom = new JSDOM(htmlString).window.document;
+        const nodes = $generateNodesFromDOM(editor, dom);
+        $getRoot().append($createParagraphNode().append(...nodes));
+      },
+      {
+        onUpdate() {
+          const editorState = editor.getEditorState();
+          resolve(JSON.stringify(editorState));
         },
-        ...elementChildren,
-      ];
-    }
-    case 'h1': {
-      const { textChildren, elementChildren } = flatFormattedText(children);
-      return [
-        { type: 'heading-one', children: textChildren },
-        ...elementChildren,
-      ];
-    }
-    case 'h2': {
-      const { textChildren, elementChildren } = flatFormattedText(children);
-      return [
-        { type: 'heading-two', children: textChildren },
-        ...elementChildren,
-      ];
-    }
-    case 'li': {
-      const { textChildren, elementChildren } = flatFormattedText(children);
-      return [
-        { type: 'list-item', children: textChildren },
-        ...elementChildren,
-      ];
-    }
-    case 'ul': {
-      const listItems = children.filter(isListItemElement);
-      return [{ type: 'bulleted-list', children: listItems }];
-    }
-    case 'ol': {
-      const listItems = children.filter(isListItemElement);
-      return [{ type: 'numbered-list', children: listItems }];
-    }
-    case 'img': {
-      if (!imageUuid) return null;
-      return [{ type: 'image', uuid: imageUuid, children: [{ text: '' }] }];
-    }
-    default: {
-      // It's missing the anchor tag, so we'll just return the text.
-      // It's missing the table related tags
-      const { textChildren, elementChildren } = flatFormattedText(children);
-      return [
-        { type: 'paragraph', children: textChildren },
-        ...elementChildren,
-      ];
-    }
-  }
+      },
+    );
+  });
+}
+
+export function replaceImage(content: string, image: string) {
+  return content.replace(/"uuid":"(?<uuid>[^"]+)/g, `"uuid":"${image}`);
 }
